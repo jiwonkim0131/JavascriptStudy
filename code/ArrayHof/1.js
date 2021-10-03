ea74605ee6482c14f9345ae947c696e40bf86f4f
JIWONKIM <qhsh866@gmail.com>
2021-10-03 16:34:30 +0900

    feat: change render,getMaxId

--- code/ArrayHof/1.js
+++ code/ArrayHof/1.js
@@ -3,18 +3,34 @@
 const todos = [
   { id: 3, content: 'HTML', completed: false },
   { id: 2, content: 'CSS', completed: true },
-  { id: 1, content: 'Javascript', completed: false }
+  { id: 1, content: 'Javascript', completed: false },
 ];
 
 const render = todos => {
+  /* Do something */
   let html = '';
+
   todos.forEach(todo => {
-    html += `<li id="${todo.id}">
-      <label><input type="checkbox"${todo.completed ? ' checked' : ''}>${
-      todo.content
-    }</label></li>`;
+    const { id, content, completed } = todo;
+    html += `
+        <li id="${id}">
+          <label><input type="checkbox" ${completed ? 'checked' : ''}> ${content}</label>
+        </li>`;
   });
 
   return html;
 };
+
+// const render = todos => {
+//   let html = '';
+//   todos.forEach(todo => {
+//     html += `<li id="${todo.id}">
+//       <label><input type="checkbox"${todo.completed ? ' checked' : ''}>${
+//       todo.content
+//     }</label></li>`;
+//   });
+
+//   return html;
+// };
+
 console.log(render(todos));
--- code/ArrayHof/9.js
+++ code/ArrayHof/9.js
@@ -3,11 +3,13 @@
 const todos = [
   { id: 3, content: 'HTML', completed: false },
   { id: 2, content: 'CSS', completed: true },
-  { id: 1, content: 'Javascript', completed: false }
+  { id: 1, content: 'Javascript', completed: false },
 ];
 
-const getMaxId = todos =>
-  todos.length ? Math.max(...todos.map(todo => todo.id)) : 0;
+// const getMaxId = todos =>
+//   todos.length ? Math.max(...todos.map(todo => todo.id)) : 0;
+
+const getMaxId = todos => Math.max(0, ...todos.map(todo => todo.id));
 
 console.log(getMaxId(todos)); // 3
 console.log(getMaxId([])); // 0
