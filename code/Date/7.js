14dadd5664fa6a545615c251d104ef9cf0a4a297
JIWONKIM <qhsh866@gmail.com>
2021-10-09 23:36:27 +0900

    feat: Add solution of Date-Q7

---
+++ code/Date/7.js
@@ -0,0 +1,6 @@
+// 7. 2개의 Date 객체가 같은 년도/월/날짜를 가리키는지 확인하기
+
+const isEqualDate = (date1, date2) => date1.getTime() === date2.getTime();
+
+console.log(isEqualDate(new Date('2021/07/24'), new Date('2021/07/24'))); // => true
+console.log(isEqualDate(new Date('2021/07/24'), new Date('2022/07/2'))); // => false
