4717dee99f3d56f84781abe30fdbf89147255b3d
JIWONKIM <qhsh866@gmail.com>
2021-10-09 23:27:56 +0900

    feat: Add solution of Date-Q3

---
+++ code/Date/3.js
@@ -0,0 +1,12 @@
+// 3. 특정 달의 말일 구하기
+
+const getLastDateOfMonth = (year, month) => new Date(year, month + 1, 0).getDate();
+
+// 2021년 1월의 마지막 날은 31일
+console.log(getLastDateOfMonth(2021, 0)); // => 31
+
+// 2021년 2월의 마지막 날은 28일
+console.log(getLastDateOfMonth(2021, 1)); // => 28
+
+// 2021년 9월의 마지막 날은 30일
+console.log(getLastDateOfMonth(2021, 8)); // => 30
